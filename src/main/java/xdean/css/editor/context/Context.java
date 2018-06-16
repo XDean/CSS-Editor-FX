96a5ff5fc56ee02b4cfc2bb4a0fea9d71dffd68c
XDean <373216024@qq.com>
2018-06-16 16:29:37 +0800

    update Context

--- src/main/java/xdean/css/editor/context/Context.java
+++ src/main/java/xdean/css/editor/context/Context.java
@@ -1,11 +1,14 @@
 package xdean.css.editor.context;
 
 import java.io.IOException;
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import javax.inject.Inject;
 
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 
 import com.sun.javafx.util.Logging;
@@ -15,17 +18,21 @@ import sun.util.logging.PlatformLogger.Level;
 import xdean.css.editor.service.DialogService;
 import xdean.jex.log.Logable;
 import xdean.jex.util.file.FileUtil;
+import xdean.jfx.spring.starter.FxContextPostProcessor;
+import xdean.spring.auto.AutoSpringFactories;
 
-// TODO should be a application listener
 @Configuration
-public class Context implements Logable {
+@AutoSpringFactories(ApplicationListener.class)
+public class Context implements FxContextPostProcessor, ApplicationListener<ApplicationStartingEvent>,
+    UncaughtExceptionHandler, Logable {
   public static final Path HOME_PATH = Paths.get(System.getProperty("user.home"), ".xdean", "css");
   public static final Path TEMP_PATH = HOME_PATH.resolve("temp");
   public static final Path LAST_FILE_PATH = Context.TEMP_PATH.resolve("last");
 
   private @Inject DialogService messageService;
 
-  public Context() {
+  @Override
+  public void onApplicationEvent(ApplicationStartingEvent event) {
     debug("Setup Css Editor Environment");
     // create directories
     try {
@@ -36,13 +43,25 @@ public class Context implements Logable {
     }
     // close CSS logger
     Logging.getCSSLogger().setLevel(Level.OFF);
+  }
+
+  @Override
+  public void beforeStart() {
     // handle uncaught exception
-    Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-      error("Uncaught exception", e);
-      if (e instanceof Error) {
-        System.exit(1);
-      }
+    Thread.setDefaultUncaughtExceptionHandler(this);
+    Platform.runLater(() -> Thread.currentThread().setUncaughtExceptionHandler(this));
+  }
+
+  @Override
+  public void uncaughtException(Thread t, Throwable e) {
+    error("Uncaught exception", e);
+    if (e instanceof Error) {
+      System.exit(1);
+    }
+    if (Platform.isFxApplicationThread()) {
+      messageService.showError("ERROR", e);
+    } else {
       Platform.runLater(() -> messageService.showError("ERROR", e));
-    });
+    }
   }
 }
